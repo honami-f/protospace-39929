1614386c480b62b6d2ecda7108317a94ab11f014
honami-f <hnmr.o.s2@gmail.com>
2023-10-10 12:16:09 +0900

    訂正

--- db/migrate/20231008065809_create_prototypes.rb
+++ db/migrate/20231008065809_create_prototypes.rb
@@ -2,7 +2,7 @@ class CreatePrototypes < ActiveRecord::Migration[7.0]
   def change
     create_table :prototypes do |t|
       t.string :title, null: false
-      t.text :cacht_copy, null: false
+      t.text :catch_copy, null: false
       t.text :concept, null: false
       t.references :user, null: false, foreign_key: true
       t.references :prototype, null: false, foreign_key: true
--- db/migrate/20231009023342_rename_cacht_copy_column_to_prototype.rb
+++
@@ -1,5 +0,0 @@
-class RenameCachtCopyColumnToPrototype < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :prototypes, :cacth_copy, :catch_copy
-  end
-end
